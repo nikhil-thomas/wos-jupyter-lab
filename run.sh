c1f23deb6a631ed32ec5a5a9d6235a09aed2438b
Nikhil Thomas <--global>
2020-04-10 18:40:33 +0530

    add learning regex

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-jupyter lab --notebook-dir notebooks --ip "0.0.0.0" #--browser "firefox"
+#jupyter lab --notebook-dir notebooks --ip "0.0.0.0" --browser "firefox"
+jupyter lab --notebook-dir notebooks --ip "0.0.0.0" 
 
